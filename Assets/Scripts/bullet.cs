c24e2f277f3d20071d7353d6bed8f3f56acaaa02
programacion <programacion@evolis3d.com>
2021-07-05 14:52:37 +0200

    Mecánica de disparo, con pooling
    He empezado con la mecánica modular acoplable de disparo, tiene una pool.
    - He creado un script para el movimiento y lógica de la bullet.
    - He hecho un prefab de Bullet , con su script correspondiente.
    
    TO-DO: disparable_pool.cs todavía está WIP, toca gestionar el reciclaje ....

---
+++ Assets/Scripts/bullet.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class bullet : MonoBehaviour
+{
+    public float damage = 10f;
+    public float speed = 10f;
+    
+    private Collider2D col;
+    private SpriteRenderer rend;
+    private bool isMoving = true;
+    private Vector2 dir;
+
+    private void Awake()
+    {
+        col = GetComponent<Collider2D>();
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
+        Reset();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (isMoving)
+        {
+            transform.Translate(dir * (speed * Time.deltaTime));
+        }
+    }
+
+    private void OnBecameInvisible()
+    {
+        Reset();
+    }
+
+    public void Reset()
+    {
+        col.enabled = false;
+        rend.enabled = false;
+        col.transform.position = Vector3.zero;
+        dir = Vector2.zero;
+        isMoving = false;
+    }
+
+    public void Fire(Vector3 pos = new Vector3(), Vector2 direc = new Vector2())
+    {
+        col.enabled = true;
+        rend.enabled = true;
+        col.transform.position = pos;
+        dir = direc;
+        isMoving = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.CompareTag("suelo")) // por ejemplo
+        {
+            Reset();
+        }
+    }
+}
---
+++ Assets/Scripts/disparable_pool.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disparable_pool : MonoBehaviour
+{
+    public GameObject prefabBullet;
+    public int poolSize = 15;
+
+    [SerializeField] private List<GameObject> pool = new List<GameObject>();
+    private int bulletsLeft;
+    
+    private void Start()
+    {
+        var PoolFolder = new GameObject("Pool-Bullets");
+        
+        for (int i = 0; i < poolSize; i++)
+        {
+            var bullet = Instantiate(prefabBullet);
+            pool.Add(bullet);
+            //pool[i].GetComponent<bullet>()...
+            pool[i].transform.SetParent(PoolFolder.transform);
+            bulletsLeft++;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            pool[bulletsLeft-1].GetComponent<bullet>().Fire();
+            bulletsLeft--;
+        }
+    }
+}

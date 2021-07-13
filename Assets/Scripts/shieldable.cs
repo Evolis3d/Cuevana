b8a7125d2b5a7c1e564f7077fbef8c61ae2175d9
programacion <programacion@evolis3d.com>
2021-07-14 01:39:20 +0200

    Mecánica modular: Escudo
    He añadido un módulo acoplable de escudo/shield a los vehículos:
    - Tiene 3 modos: Barrier, Absorb y Reflect.
    - Se mantiene activado con un botón, y se cambia su modo con otro.
    
    TO-DO: mapear bien el control, y programar la lógica de cada modo de shield.

---
+++ Assets/Scripts/shieldable.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+
+public class shieldable : MonoBehaviour
+{
+    public GameObject prefabShield;
+    public bool shieldOn=false;
+
+    private GameObject shield;
+    private SpriteRenderer ShieldRend;
+    Color modo1 = Color.cyan * new Color(0,1,1,0.25f);
+    Color modo2 = Color.red * new Color(1,0,0,0.25f);
+    Color modo3 = Color.green * new Color(0,1,0,0.25f);
+    
+    private int currentMode = 1;
+
+    private void Start()
+    {
+        shield = Instantiate(prefabShield, transform.position, Quaternion.identity);
+        ShieldRend = shield.GetComponent<SpriteRenderer>();
+        shield.transform.SetParent(transform);
+        SetMode(0);
+
+        ToggleShield(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Shield"))
+        {
+            if (shieldOn!=true) ToggleShield(true);
+        } else if (Input.GetButtonUp("Shield"))
+        {
+            if (shieldOn != false) ToggleShield(false);
+        }
+        //switches between shield modes
+        //if (Input.GetButtonDown())
+        
+    }
+
+    private void SetMode(int mode)
+    {
+        mode = mode % 3;
+        switch (mode)
+        {
+            case 0:
+                ShieldRend.color = modo1;
+                break;
+            case 1:
+                ShieldRend.color = modo2;
+                break;
+            case 2:
+                ShieldRend.color = modo3;
+                break;
+        }
+    }
+
+    private void ToggleShield(bool onOff)
+    {
+        shieldOn = onOff;
+        ShieldRend.enabled = onOff;
+        var sComp = shield.GetComponent<CircleCollider2D>();
+        sComp.enabled = onOff;
+    }
+
+}

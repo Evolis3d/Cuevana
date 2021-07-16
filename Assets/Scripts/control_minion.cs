5ba6cb865b3ece4e59e8ef6fa056cc7b16b43f3f
programacion <programacion@evolis3d.com>
2021-07-16 09:28:49 +0200

    Minion camina...
    Pulidos los controles del minion para que sólo camine y salte cuando esté tocando el suelo... WIP
    
    TO-DO : Averiguar por qué coj... deja saltar incluso en el aire una 2ª vez...

--- Assets/Scripts/control_minion.cs
+++ Assets/Scripts/control_minion.cs
@@ -1,12 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Net;
+using UnityEngine;
 
 public class control_minion : MonoBehaviour
 {
     private SpriteRenderer _spr;
     private Animator _anim;
     private Rigidbody2D _rb;
+   
     public float speed = 1f;
     public float jumpforce = 5f;
+
+    private bool onGround;
+    
     private void Awake()
     {
         _spr = GetComponent<SpriteRenderer>();
@@ -16,24 +21,41 @@ public class control_minion : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") != 0)
+        if (onGround)
         {
-            if (_anim.GetBool("isMoving")!=true) _anim.SetBool("isMoving",true);
+            if (Input.GetAxisRaw("Horizontal") != 0)
+            {
+                if (_anim.GetBool("isMoving")!=true) _anim.SetBool("isMoving",true);
             
-            //muevo
-            var lado = Input.GetAxisRaw("Horizontal");
-            _spr.flipX = lado >= 1;  
-            transform.localPosition += Vector3.left * (-lado * speed * Time.deltaTime);
+                //muevo
+                var lado = Input.GetAxisRaw("Horizontal");
+                _spr.flipX = lado >= 1;  
+                transform.localPosition += Vector3.left * (-lado * speed * Time.deltaTime);
+            }
+            else
+            {
+                if (_anim.GetBool("isMoving")!=false) _anim.SetBool("isMoving",false);
+            }
+        
+            if ((Input.GetButtonDown("Thrust")) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                if (onGround != true) return;
+                
+                _rb.AddForce(transform.up * jumpforce, ForceMode2D.Force);
+                onGround = false;
+            }  
         }
         else
         {
-            if (_anim.GetBool("isMoving")!=false) _anim.SetBool("isMoving",false);
+            var layermask = LayerMask.GetMask("suelos");
+            var ray = transform.TransformDirection(Vector2.down);
+            var hit = Physics2D.Raycast(transform.position, ray,0.25f,layermask);
+            if (hit.collider != null)
+            {
+                onGround = true;
+            }
         }
-        
-        if ((Input.GetButtonDown("Thrust")) || Input.GetKeyDown(KeyCode.UpArrow))
-        {
-           _rb.AddForce(transform.up * jumpforce,ForceMode2D.Force); 
-        }
+
     }
     
 }

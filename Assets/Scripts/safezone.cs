914af467169941a0720461c479100417aca9e416
programacion <programacion@evolis3d.com>
2021-07-24 03:37:50 +0200

    safezone fixed!
    Script mucho más eficiente, para instanciar "rescatados" con cierta frecuencia, sólo si la nave ha aterrizado correctamente...
    
    TO-DO: Seguir investigando lo del "landed" de la nave. A ver cómo se lo digo...

--- Assets/Scripts/safezone.cs
+++ Assets/Scripts/safezone.cs
@@ -1,26 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class safezone : MonoBehaviour
 {
     public GameObject prefabRescued;
-    private float _freqspawn=2f;
+    private float _freqspawn=1f;
+    private float _timer;
+    private bool _naveLanded;
+    private Vector2 _posnave;
+
+
+    private void Start()
+    {
+        _timer = 0f;
+        _naveLanded = false;
+    }
+
+    private void Update()
+    {
+        if (!_naveLanded) return;
+        
+        if (_timer > _freqspawn)
+        {
+            if (prefabRescued) Instantiate(prefabRescued, _posnave, Quaternion.identity);
+            _timer = 0f;
+        }
+        else
+        {
+            _timer += 1f * Time.deltaTime;
+        }
+    }
 
     private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("nave"))
         {
             var naveComp = other.transform.GetComponent<control_nave>();
-            if (naveComp.landed)
-            {
-                StartCoroutine(SpawnRescued(other.transform,_freqspawn));
-            }
+            _naveLanded = naveComp.landed;
+            _posnave = other.transform.position;
         }
     }
 
-    IEnumerator SpawnRescued(Transform parent, float delay=1f)
+    private void OnTriggerExit2D(Collider2D other)
     {
-        yield return new WaitForSeconds(delay);
-        if (prefabRescued) Instantiate(prefabRescued, parent.position, Quaternion.identity);
+        _naveLanded = false;
     }
 }

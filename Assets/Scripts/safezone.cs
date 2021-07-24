7d4e264ad84ae34afc56fe2aa92116f017a82b96
programacion <programacion@evolis3d.com>
2021-07-24 03:01:55 +0200

    safezone, lógica
    He creado el prefab y el script para la base aliada:
    - Cuando detecte una nave posada, spawnea Minions "rescatados".
    
    TO-DO: Hacer el prefab y script de la variante de Minion "rescatado".

---
+++ Assets/Scripts/safezone.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class safezone : MonoBehaviour
+{
+    public GameObject prefabRescued;
+    private float _freqspawn=2f;
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("nave"))
+        {
+            var naveComp = other.transform.GetComponent<control_nave>();
+            if (naveComp.landed)
+            {
+                StartCoroutine(SpawnRescued(other.transform,_freqspawn));
+            }
+        }
+    }
+
+    IEnumerator SpawnRescued(Transform parent, float delay=1f)
+    {
+        yield return new WaitForSeconds(delay);
+        if (prefabRescued) Instantiate(prefabRescued, parent.position, Quaternion.identity);
+    }
+}

dbd9cb56bfeab23c8fbd6cb0f6c39cf2813858e9
programacion <programacion@evolis3d.com>
2021-07-25 00:19:55 +0200

    Nueva Feature: Game Modes!
    Estoy creando la lógica de distintos modos de juego, para crear situaciones:
    - El modo por defecto es "Rescate de Prisioneros", aka Choplifter C64.
    
    TO-DO: Restar 1 vida cuando se destruya la nave.

---
+++ Assets/Scripts/GameMode.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+
+public class GameMode : MonoBehaviour
+{
+    [Header("Game")]
+    public static bool AllRescued;
+    public static DateTime TimePlayed;
+    private DateTime _startTime;
+    private DateTime _endTime;
+
+    [Header("Nave")]
+    public static bool PlayerLanded;
+    public static int Lives;
+
+    [Header("Prisioneros")]
+    public static int TotalPrisoners;
+    public static int PrisonersNeeded;
+    public static int PrisonersRescued;
+    public static int PrisonersAboard;
+    public static int PrisonersLeft;
+    public static int PrisonersKilled;
+
+    private void Start()
+    {
+        _startTime = new DateTime();
+        _endTime = new DateTime();
+        TimePlayed = new DateTime();
+        //
+        _startTime = DateTime.Now;
+        Lives = 3;
+    }
+
+    private void Update()
+    {
+        AllRescued = PrisonersRescued > PrisonersNeeded;
+        
+        if (Lives<1) GameOver(0);
+    }
+
+    private void GameOver(int result)
+    {
+        _endTime = DateTime.Now;
+        
+        switch (result)
+        {
+            case 0:     // 0 - pierde todas las vidas
+                Debug.Log("GAME OVER! No te quedan más vidas!");
+                Debug.Break();
+                break;
+            case 1:     // 1 - gana la partida
+                Debug.Log("YOU WIN! Level Complete!");
+                Debug.Break();
+                break;
+            case 2:     // 2 - mueren más minions de los necesarios para completar el nivel
+                Debug.Log("GAME OVER! Murieron más prisioneros de los necesarios!");
+                Debug.Break();
+                break;
+            case 3:     // 3 - pierdes, otros motivos
+                Debug.Log("GAME OVER! Otros motivos...");
+                Debug.Break();
+                break;
+        }
+    }
+}

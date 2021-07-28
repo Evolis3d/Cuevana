9f24627ab7686433fa6629ae454bda9a970fdc44
programacion <programacion@evolis3d.com>
2021-07-29 01:22:29 +0200

    Nueva Feature: SpawnPoint de Prisioneros!
    El script minion_spawnpoint pone un sprite de escombro en base al original, y crea unos prisioneros, si se especifica:
    - Los escombros y prisioneros se crean justo cuando se destruye el elemento.
    - He puesto un random para separar un poco a los prisioneros entre sí...
    
    TO-DO: Especificar que los escombros son estáticos y se crean dentro de su parent.

---
+++ Assets/Scripts/minion_spawnpoint.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class minion_spawnpoint : MonoBehaviour
+{
+    public Sprite crashSprite;
+    [Header("Spawn")] 
+    public GameObject prefabPickables;
+    public int amount = 3;
+
+    private void OnDestroy()
+    {
+        if (crashSprite)
+        {
+            var crash = new GameObject();
+            var spr = crash.AddComponent<SpriteRenderer>();
+            spr.sprite = crashSprite;
+            spr.sortingOrder = -1;
+            crash.transform.position = transform.position;
+            crash.isStatic = true;
+        }
+
+        if (amount > 0 && prefabPickables)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                var clon = Instantiate(prefabPickables,transform.position, Quaternion.identity);
+                var rnd = Random.Range(-0.5f, 0.5f);
+                clon.transform.Translate(rnd,0,0);
+            }
+        }
+    }
+}

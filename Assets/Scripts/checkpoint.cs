28835986af64d875668558225909becd0dc01ab4
programacion <programacion@evolis3d.com>
2021-07-30 22:41:12 +0200

    Exitable fixed!
    He decidido separar el modulo de Entrar/Salir en dos independientes, para hacer más fácil la gestión y que los vehículos sólo permitan salir, y el Minion sólo pueda entrar:
    - En cuanto sale de una nave, spawnea un Minion Playable, o lo que corresponda.
    - En cuanto sale, desactivo y quito componentes no necesarios para la nave (WIP).
    
    TO-DO: Hacer más eficiente la desactivación de cosas. Usar "Desactiveitor", maybe?

--- Assets/Scripts/checkpoint.cs
+++ Assets/Scripts/checkpoint.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Timers;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class checkpoint : MonoBehaviour
 {
--- Assets/Scripts/exitable.cs
+++ Assets/Scripts/exitable.cs
@@ -2,17 +2,40 @@
 
 public class exitable : MonoBehaviour
 {
-    // Update is called once per frame
+    public GameObject prefabPlayable;
+    private control_nave _control;
+    private bool _canLeave;
+
+    private void Awake()
+    {
+        _control = GetComponent<control_nave>();
+    }
+
     void Update()
     {
-        if (Input.GetAxisRaw("Enter/Exit Vehicle") <= -1)
+        _canLeave = _control.landed;
+
+        if (_canLeave)
         {
-            Debug.Log("Sale del vehiculo!");
-            Debug.Break();
-        }else if (Input.GetAxisRaw("Enter/Exit Vehicle") >= 1)
-        {
-            Debug.Log("Entra al vehiculo!");
-            Debug.Break();
+            if (Input.GetAxisRaw("Enter/Exit Vehicle") <= -1)
+            {
+                if (!prefabPlayable) return;
+
+                var navePos = transform.position;
+                
+                Destroy(gameObject.GetComponent<sensores>());
+                Destroy(gameObject.GetComponent<control_nave>());
+                Destroy(gameObject.GetComponent<control_pod>());
+                Destroy(gameObject.GetComponent<exitable>());
+                gameObject.GetComponent<Rigidbody2D>().simulated = false;
+                gameObject.GetComponent<Collider2D>().isTrigger = true;
+                
+                var miniYo = Instantiate(prefabPlayable, navePos, Quaternion.identity);
+                //Destroy(gameObject);
+                
+                //Debug.Log("Sale del vehiculo!");
+                //Debug.Break();
+            }
         }
     }
 }

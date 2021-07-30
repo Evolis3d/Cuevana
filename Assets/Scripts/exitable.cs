b674086a691bad152bc29faf92d6a3e140561fe3
programacion <programacion@evolis3d.com>
2021-07-30 21:52:02 +0200

    Mecánica modular: entrar/salir de vehículos!
    He empezado una mecánica modular acoplable para poder y salir de vehículos, siempre que hayan aterrizado y/o estén parados:
    - Aplicado de momento a la Nave-Prototipo (WIP).
    - Sólo detecta y pausa el juego, nada más (WIP).
    
    TO-DO: Programar la lógica y dependencias de este módulo.

---
+++ Assets/Scripts/exitable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class exitable : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Enter/Exit Vehicle") <= -1)
+        {
+            Debug.Log("Sale del vehiculo!");
+            Debug.Break();
+        }else if (Input.GetAxisRaw("Enter/Exit Vehicle") >= 1)
+        {
+            Debug.Log("Entra al vehiculo!");
+            Debug.Break();
+        }
+    }
+}

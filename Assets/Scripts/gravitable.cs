d856c35e4759c381768abdebbbf6920ce2dccaaf
programacion <programacion@evolis3d.com>
2021-08-14 01:31:43 +0200

    gravitable fixed!
    He parametrizado el giro y las gravedades por defecto, para recuperar valores correspondientes a cada bicho o vehículo del juego mientras estén flotando.
    
    TO-DO: Hacer que los Minion floten animadamente...

--- Assets/Scripts/gravitable.cs
+++ Assets/Scripts/gravitable.cs
@@ -4,69 +4,77 @@
 public class gravitable : MonoBehaviour
 {
     //privadas
-    private Rigidbody2D rb;
-    private Collider2D col;
+    private Rigidbody2D _rb;
+    private Collider2D _col;
+
+    [SerializeField] private float giroFlotando = 10f;
+    private float _defaultGravity;
     
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
-        col = GetComponent<Collider2D>();
+        _rb = GetComponent<Rigidbody2D>();
+        _col = GetComponent<Collider2D>();
+    }
+
+    private void Start()
+    {
+        _defaultGravity = _rb.gravityScale;
     }
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        if (!rb || !col) return;
+        if (!_rb || !_col) return;
         //if (col is BoxCollider2D boxCollider2D) col = boxCollider2D;
         
         if (other.CompareTag("0gravzone"))
         {
             var puntos = new ContactPoint2D[0];
-            col.GetContacts(puntos);
+            _col.GetContacts(puntos);
             foreach (var punto in puntos)
             {
                 if (!other.OverlapPoint(punto.point)) return;
             }
             
-            if (rb.gravityScale !=0f) rb.gravityScale = 0f; //por defecto la nave tiene 0.1
-            if (rb.freezeRotation) rb.freezeRotation = false;
-            rb.angularVelocity = 10f;  //gira por ahí...
+            if (_rb.gravityScale !=0f) _rb.gravityScale = 0f; //por defecto, cada control tiene su propio peso...
+            if (_rb.freezeRotation) _rb.freezeRotation = false;
+            _rb.angularVelocity = giroFlotando;  //gira por ahí...
             return;
         }
 
         if (other.CompareTag("invertgravzone"))
         {
             var puntos = new ContactPoint2D[0];
-            col.GetContacts(puntos);
+            _col.GetContacts(puntos);
             foreach (var punto in puntos)
             {
                 if (!other.OverlapPoint(punto.point)) return;
             }
             
-            if (rb.gravityScale != -0.1f) rb.gravityScale = -0.1f; //por defecto la nave tiene 0.1
+            if (_rb.gravityScale != (-1f * _defaultGravity)) _rb.gravityScale = (-1f * _defaultGravity); //por defecto la nave tiene 0.1
 
-            if (rb.rotation!=180f) rb.rotation = Mathf.LerpAngle(rb.rotation, 180f, 5f * Time.deltaTime);
+            if (_rb.rotation!=180f) _rb.rotation = Mathf.LerpAngle(_rb.rotation, 180f, 5f * Time.deltaTime);
         }
 
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (!rb || !col) return;
+        if (!_rb || !_col) return;
         
         if (other.CompareTag("0gravzone"))
         {
-            rb.gravityScale = 0.1f; //por defecto la nave tiene 0.1
-            if (!rb.freezeRotation) rb.freezeRotation = true;
-            rb.angularVelocity = 0f;
-            rb.rotation = 0f;
+            _rb.gravityScale = _defaultGravity;
+            if (!_rb.freezeRotation) _rb.freezeRotation = true;
+            _rb.angularVelocity = 0f;
+            _rb.rotation = 0f;
             return;
         }
 
         if (other.CompareTag("invertgravzone"))
         {
-            rb.gravityScale = 0.1f; //por defecto la nave tiene 0.1 y hacia abajo..
+            _rb.gravityScale = _defaultGravity;
 
-            if (rb.rotation != 0f) rb.rotation = 0f;
+            if (_rb.rotation != 0f) _rb.rotation = 0f;
         }
     }
 }

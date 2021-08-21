5b8d413564197050ccb3e2cbd9d3c053524a3e5c
programacion <programacion@evolis3d.com>
2021-08-21 17:33:04 +0200

    Mecánica Modular : IA_disparable!
    He creado un script acoplable para que las IA de NPC y enemigos puedan disparar usando el Bullet_Pool:
    - Funciona con los mismos parámetros que paso con el disparable.cs (WIP).
    - Dispara el mismo tipo de proyectil común de momento (WIP).
    
    - Crear un enemigo para que dispare y arreglar el origen del disparo...

---
+++ Assets/Scripts/IA_disparable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class IA_disparable : MonoBehaviour
+{
+    private void Start()
+    { }
+
+    public void Shoot(Transform from)
+    {
+            GameSystems.bulletPool.Shoot(from);
+    }
+}

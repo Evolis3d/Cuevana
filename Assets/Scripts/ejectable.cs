776f1553efc22f6fa0c342442341d4f77365a0eb
programacion <programacion@evolis3d.com>
2021-08-27 17:23:46 +0200

    Mecánica modular: Eyección de vehículos!
    He hecho más genérico el control_pod y lo he convertido en una mecánica aplicable a cualquier vehículo:
    - Al pulsar el botón correspondiente, se instancia un "vehículo" o "cosa" intermedia.
    - El vehículo actual pierde el control y se autodestruirá en 3! segundos (WIP).
    
    TO-DO: Cambiar los prefabs de los vehículos que usen "control_pod" para que ahora usen el "ejectable", con los mismos parámetros...

---
+++ Assets/Scripts/ejectable.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class ejectable : MonoBehaviour
+{
+    [Header("escapePod")] 
+    public GameObject prefabEjectable;
+
+    [Header("Control Principal")] 
+    public float selfDestruction = 3f;
+    private controllable cComp;
+
+    private void Awake()
+    {
+        cComp = GetComponent<controllable>();
+        if (!cComp) throw new Exception("Error! Ejectable depende de vehículo controllable!");
+        if (!prefabEjectable) throw new Exception("Error! Ejectable requiere de un prefab de escape!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Tecla de eyección, de momento SpaceBar
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameMode.Lives++;
+            
+            if (prefabEjectable) Instantiate(prefabEjectable, transform.position, Quaternion.identity);
+            
+            //pierdo el control de la nave...
+            Destroy(gameObject,selfDestruction);
+            GameMode.PrisonersKilled += GameMode.PrisonersAboard;
+            GameMode.PrisonersAboard = 0;
+            cComp.enabled = false;
+        }
+    }
+
+}
